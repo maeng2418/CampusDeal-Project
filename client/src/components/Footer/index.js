f4ba5b86c4214861fe10d7b63b776df2e244d5de
maeng2418 <maeng2418@github.com>
2020-04-07 16:34:37 +0900

    1. Boilerplate

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Footer = () => {
+  return (
+<footer className={styles.footer}>
+        <div className={styles.container}>
+            <div className={styles.row}>
+                <div className={styles.col}>
+                    <div className={styles.smallContainer}>
+                        <small>
+                        (주) 캠퍼스 딜 | 대표 김명성 | 사업자등록번호 111-82-12345 | 통신판매신고 제2019-서울마포-0000호
+                        </small>
+                    </div>
+                    <div>
+                        <small>
+                        서울시 노원구 공릉로 154-15 | 고객센터 02-1544-1234 | FAX 050-4130-1234
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/RegisterPage/container.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import RegisterPage from './presenter';
+
+const Container = (props) => {
+
+  const onSubmitHandler = (body) => {
+
+    if(body.password !== body.confirmPassword) {
+      return alert('비밀번호와 비밀번호 확인은 같아야 합니다.');
+    }
+
+    props.registerUser(body)
+    .then(response => {
+      if(response.payload.success){
+        props.history.push('/login');
+      } else {
+        if(response.payload.err.code === 11000){
+          alert("이미 존재하는 ID입니다.");
+        } else {
+          alert("Failed to sign up");
+        }
+      }
+    })
+  }
+
+  return (
+    <RegisterPage onSubmitHandler={onSubmitHandler} />
+  );
+}
+
+export default withRouter(Container);
